b8af44f9c40ec33087ebc50f603e193e0fd7fac1
Adrian Riedel <adrian.riedel@sap.com>
2020-06-07 01:18:24 +0200

    add simple http client

---
+++ src/client/httpclient.go
@@ -0,0 +1,26 @@
+package client
+
+import (
+	"bytes"
+	"net/http"
+)
+
+const baseURL = "http://localhost:1337/"
+
+// Do does what do does
+func Do(method string, endpoint string, body []byte) error {
+	url := baseURL + endpoint
+
+	request, err := http.NewRequest(method, url, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+
+	client := http.Client{}
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
