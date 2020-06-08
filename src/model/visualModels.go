73b944bbe8cd3afcd84accda5d2aa9dd39758485
Adrian Riedel <adrian.riedel@eynorey.com>
2020-06-08 23:16:40 +0200

    use templates for html generation, add controlpanel endpoint

--- src/model/visualModels.go
+++ src/model/visualModels.go
@@ -4,10 +4,26 @@ package model
 type Action string
 
 const (
+	// TriggerVisualAction triggers a visual effect
+	TriggerVisualAction = Action("trigger")
 	// StartVisualAction starts a visual effect
 	StartVisualAction = Action("start")
-	// StopVisualAction stops a visual effect
-	StopVisualAction = Action("stop")
 	// RestartVisualAction restarts a visual effect
 	RestartVisualAction = Action("restart")
+	// StopVisualAction stops a visual effect
+	StopVisualAction = Action("stop")
 )
+
+// VisualControl contains information about an effect used for templates
+type VisualControl struct {
+	Name         string
+	AllowTrigger bool
+	AllowStart   bool
+	AllowRestart bool
+	AllowStop    bool
+}
+
+// ControlPanel holds all data to be rendered on the control panel website
+type ControlPanel struct {
+	Effects []VisualControl
+}
