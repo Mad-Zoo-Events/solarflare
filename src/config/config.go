eb60d59ef279df4ecbca2a674f257270fe91afac
Adrian Riedel <adrian.riedel@eynorey.com>
2020-06-11 22:32:31 +0200

    use encryption for talking to eyecandy

--- src/config/config.go
+++ src/config/config.go
@@ -1,21 +1,30 @@
 package config
 
-import "github.com/eynorey/candyshop/src/model"
+import (
+	"os"
+
+	"github.com/eynorey/candyshop/src/model"
+)
+
+const envEncryptionKey = "ENCRYPTION_KEY"
 
 // Config contains the server configuration
 type Config struct {
 	ParticleEffects []model.ParticleEffectControl
 	Dragon          model.DragonControl
 	Servers         []model.Server
+	EncryptionKey   string
 }
 
 var cfg Config
 
 func init() {
+	encryptionKey := os.Getenv(envEncryptionKey)
 	cfg = Config{
 		ParticleEffects: loadParticleEffectControls(),
 		Dragon:          loadDragonControl(),
 		Servers:         loadServers(),
+		EncryptionKey:   encryptionKey,
 	}
 }
 
