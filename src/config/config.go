03fabf62250f4e3d22d161701add14e1b1db716d
Adrian Riedel <adrian.riedel@eynorey.com>
2020-06-15 00:54:11 +0200

    load presets from database

--- src/config/config.go
+++ src/config/config.go
@@ -1,34 +1,19 @@
 package config
 
 import (
-	"os"
-
 	"github.com/eynorey/candyshop/src/model"
 )
-
-const envEncryptionKey = "ENCRYPTION_KEY"
 
 // Config contains the server configuration
 type Config struct {
 	ParticleEffectPresets []model.ParticleEffectPreset
 	DragonEffectPresets   []model.DragonEffectPreset
 	Servers               []model.Server
-	EncryptionKey         string
 }
 
 var cfg Config
 
-func init() {
-	encryptionKey := os.Getenv(envEncryptionKey)
-	cfg = Config{
-		ParticleEffectPresets: loadParticleEffectPresets(),
-		DragonEffectPresets:   loadDragonEffectPresets(),
-		Servers:               loadServers(),
-		EncryptionKey:         encryptionKey,
-	}
-}
-
 // Get returns the server config
-func Get() Config {
-	return cfg
+func Get() *Config {
+	return &cfg
 }
