98598b5b3545858eebc3be5b90461f60eac0f42a
Adrian Riedel <adrian.riedel@eynorey.com>
2020-06-14 14:53:28 +0200

    http client/error handling cleanup

--- src/client/httpclient.go
+++ src/client/httpclient.go
@@ -3,8 +3,10 @@ package client
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"sync"
 	"time"
@@ -13,64 +15,59 @@ import (
 	"github.com/eynorey/candyshop/src/utils"
 )
 
-var (
-	errCount int
-	respErr  error
-)
-
 // SendEffectRequest send a effect request to all registeres eyecandy instances
 func SendEffectRequest(endpoint string, request interface{}) error {
+	cfg := config.Get()
+	c := http.Client{
+		Timeout: time.Duration(1 * time.Second),
+	}
+
 	body, err := json.Marshal(request)
 	if err != nil {
 		return utils.HandleError("Failed to marshal request", err)
 	}
 
-	cfg := config.Get()
-	c := http.Client{
-		Timeout: time.Duration(5 * time.Second),
-	}
-
 	var wg sync.WaitGroup
-	errCount = 0
-	respErr = nil
+	errCount := 0
+
 	for _, server := range cfg.Servers {
 		url := server.Address + endpoint
 		wg.Add(1)
-		go sendEffectRequest(c, url, &body, &wg)
+		go sendEffectRequest(c, url, &body, &wg, &errCount)
 	}
 
 	wg.Wait()
 
-	if respErr != nil {
+	if errCount > 0 {
 		if errCount == len(cfg.Servers) {
-			return utils.HandleError("Command failed on all servers", respErr)
+			return errors.New("Command failed on all servers")
 		}
-		return utils.HandleError(fmt.Sprintf("Command failed on %d out of %d servers", errCount, len(cfg.Servers)), respErr)
+		return fmt.Errorf("Command failed on %d out of %d servers", errCount, len(cfg.Servers))
 	}
 
 	return nil
 }
 
-func sendEffectRequest(c http.Client, url string, body *[]byte, wg *sync.WaitGroup) {
+func sendEffectRequest(c http.Client, url string, body *[]byte, wg *sync.WaitGroup, errCount *int) {
 	defer wg.Done()
 	request, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(*body))
 	if err != nil {
-		respErr = fmt.Errorf("Error compiling request to Eyecandy: %s", err.Error())
-		errCount++
+		log.Printf("Error compiling request to Eyecandy: %s", err.Error())
+		*errCount++
 		return
 	}
 
 	response, err := c.Do(request)
 	if err != nil {
-		respErr = fmt.Errorf("Error sending request to Eyecandy: %s", err.Error())
-		errCount++
+		log.Printf("Error sending request to Eyecandy: %s", err.Error())
+		*errCount++
 		return
 	}
 
 	if response.StatusCode < 200 || response.StatusCode >= 400 {
 		responseMessage, _ := ioutil.ReadAll(response.Body)
-		respErr = fmt.Errorf("Error received from Eyecandy: %s", string(responseMessage))
-		errCount++
+		log.Printf("Error received from Eyecandy: %s", string(responseMessage))
+		*errCount++
 		return
 	}
 }
