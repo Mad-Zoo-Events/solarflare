22da41c99c0822ff0caf5ba4ff19f778d46f002e
Adrian Riedel <adrian.riedel@eynorey.com>
2020-06-14 14:53:28 +0200

    http client/error handling cleanup

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -8,6 +8,8 @@ import (
 // HandleError writes a complete error log and returns a new error with
 // the consumer-friendly message passed as 'msg'
 func HandleError(msg string, err error) error {
-	log.Printf("%s: %s", msg, err.Error())
+	if err != nil {
+		log.Printf("%s: %s", msg, err.Error())
+	}
 	return errors.New(msg)
 }
