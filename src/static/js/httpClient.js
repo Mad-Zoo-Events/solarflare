22d4378b4908a80368f27fd05dd4fbfb204e89dc
Adrian Riedel <adrian.riedel@eynorey.com>
2020-06-14 21:07:28 +0200

    update UI following backend changes to preset model

--- src/static/js/httpClient.js
+++ src/static/js/httpClient.js
@@ -1,69 +1,31 @@
 const BASE_URL = `${window.location.origin}`;
-const PARTICLE_ENDPOINT = `${BASE_URL}/effects/particle`;
-const DRAGON_ENDPOINT = `${BASE_URL}/effects/dragon`;
-const STATUS_ENDPOINT = `${BASE_URL}/status`;
 
-sendParticleEffect = async (effectName, effectDisplayName, action) => {
-    const requestBody = {
-        "EffectName": effectName,
-        "Action": action
-    };
-
-    doEffectRequest(
-        PARTICLE_ENDPOINT,
-        JSON.stringify(requestBody),
-        (success, errorMsg) => {
-            addToLog(action, effectDisplayName, errorMsg);
-            if (success) {
-                counter(effectName, action);
-            }
-        }
-    );
-}
-
-sendDragonEffect = async (effectDisplayName, action) => {
-    const floatUp = document.getElementById("dragon-float-up").checked;
-    const requestBody = {
-        "Action": action,
-        "Static": !floatUp
-    };
-
-    doEffectRequest(
-        DRAGON_ENDPOINT,
-        JSON.stringify(requestBody),
-        (success, errorMsg) => {
-            addToLog(action, effectDisplayName, errorMsg);
-            if (success) {
-                counter("dragon", action);
-            }
-        }
-    );
-}
-
-doEffectRequest = async (endpoint, payload, callback) => {
+executePreset = async (id, displayName, action) => {
     const request = new XMLHttpRequest();
-    request.open("POST", endpoint);
-    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    request.open("POST", `${BASE_URL}/effects/${id}/${action}`);
     request.addEventListener('load', () => {
-        if (request.status >= 200 && request.status < 300) {
-            callback(true);
+        if (request.status >= 200 && request.status < 400) {
+            addToLog(action, displayName);
+            counter(id, action);
         } else {
-            callback(false, `${request.responseText}`);
+            addToLog(action, displayName, request.responseText);
         }
     });
-    request.send(payload);
+
+    request.send();
 }
 
 doStatusUpdateRequest = async () => {
     const request = new XMLHttpRequest();
-    request.open("GET", STATUS_ENDPOINT);
+    request.open("GET", `${BASE_URL}/status`);
     request.addEventListener('load', () => {
         if (request.status === 200) {
             const resp = JSON.parse(request.responseText);
             updateStatus(resp.RegisteredServerCount);
         } else {
-            addToLog(">>>", "Retrieve status update", request.responseText);
+            addToLog("[BACKEND]", "Retrieve status update", request.responseText);
         }
     });
+
     request.send();
 }
