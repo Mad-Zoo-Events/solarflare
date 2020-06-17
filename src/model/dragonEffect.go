4d19f586facd9e7fc0c964ff0e1a64bcccd82ff7
Adrian Riedel <adrian.riedel@eynorey.com>
2020-06-17 20:19:33 +0200

    create models for the timeshift effect

--- src/model/dragonEffect.go
+++ src/model/dragonEffect.go
@@ -2,13 +2,9 @@ package model
 
 // DragonEffectPreset is the request model for particle effects
 type DragonEffectPreset struct {
-	// Unique identifier of the preset
-	ID string `json:"id" form:"id"`
-	// Display name for the UI
-	DisplayName string `json:"displayName" form:"displayName"`
-	// Description of the preset for the UI
-	Description string `json:"description" form:"description"`
-	// List of dragon effects which are part of this preset
+	ID            string         `json:"id" form:"id"`
+	DisplayName   string         `json:"displayName" form:"displayName"`
+	Description   string         `json:"description" form:"description"`
 	DragonEffects []DragonEffect `json:"dragonEffects" form:"effect"`
 }
 
