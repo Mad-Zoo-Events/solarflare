944f8f887a5eab958460008afec54ae3e31296f2
Adrian Riedel <adrian.riedel@eynorey.com>
2020-06-21 18:57:39 +0200

    load potion preset upon launch

--- src/main.go
+++ src/main.go
@@ -19,6 +19,7 @@ func load() {
 	cfg.DragonEffectPresets = client.GetDragonEffectPresets()
 	cfg.TimeshiftEffectPresets = client.GetTimeshiftEffectPresets()
 	cfg.TimeshiftEffectPresets = client.GetTimeshiftEffectPresets()
+	cfg.PotionEffectPresets = client.GetPotionEffectPresets()
 	cfg.Servers = []model.Server{
 		model.Server{
 			// Address: "http://3.133.229.78:8001", //hospital public
