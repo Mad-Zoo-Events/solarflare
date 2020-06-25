35c8885c6af2d109fde15318d8afc416ee364d9d
Adrian Riedel <adrian.riedel@eynorey.com>
2020-06-26 01:30:02 +0200

    connect frontend to backend clock

--- src/static/js/httpClient.js
+++ src/static/js/httpClient.js
@@ -3,6 +3,8 @@ const STATUS_ENDPOINT = `${BASE_URL}/status`;
 const EFFECTS_ENDPOINT = `${BASE_URL}/effects`;
 const PRESETS_ENDPOINT = `${BASE_URL}/presets`;
 
+const CLOCK_ENDPOINT = `${BASE_URL}/clock`;
+
 const CP_ENDPOINT = `${BASE_URL}/controlpanel`;
 const CP_PRESETS_ENDPOINT = `${CP_ENDPOINT}/presets`;
 
@@ -48,6 +50,22 @@ doDeletePreset = async (id, effectType) => {
             location.reload();
         }
     });
+
+    request.send();
+}
+
+doSetClockSpeed = async (bpm, multiplier) => {
+    const request = new XMLHttpRequest();
+    request.open("PUT", `${CLOCK_ENDPOINT}/${bpm}/${multiplier}`);
+
+    request.send();
+}
+
+doClockSubscription = async (id, effectType, action) => {
+    const method = action === "subscribe" ? "POST" : "DELETE"
+
+    const request = new XMLHttpRequest();
+    request.open(method, `${CLOCK_ENDPOINT}/${effectType}/${id}`);
 
     request.send();
 }
