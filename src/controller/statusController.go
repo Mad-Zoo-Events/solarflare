2cb697e78476dd0f97003dee0156101b3a150186
Adrian Riedel <adrian.riedel@eynorey.com>
2020-06-26 02:27:30 +0200

    make status update return clock info + consume on UI

--- src/controller/statusController.go
+++ src/controller/statusController.go
@@ -3,13 +3,17 @@ package controller
 import (
 	"github.com/eynorey/solarflare/src/config"
 	"github.com/eynorey/solarflare/src/model"
+	"github.com/eynorey/solarflare/src/utils/clock"
 )
 
 // GetStatus returns information on the service and network status
 func GetStatus() model.StatusResponse {
 	cfg := config.Get()
+	bpm, multiplier := clock.GetSpeed()
 
 	return model.StatusResponse{
 		RegisteredServerCount: len(cfg.Servers),
+		ClockSpeedBPM:         bpm,
+		ClockSpeedMultiplier:  multiplier,
 	}
 }
