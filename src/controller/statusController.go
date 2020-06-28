cda8bcfe060ae866d68bd50059c624791a1caad0
Adrian Riedel <adrian.riedel@eynorey.com>
2020-06-28 12:39:44 +0200

    refactor clock into manager

--- src/controller/statusController.go
+++ src/controller/statusController.go
@@ -2,14 +2,14 @@ package controller
 
 import (
 	"github.com/eynorey/solarflare/src/config"
+	"github.com/eynorey/solarflare/src/manager"
 	"github.com/eynorey/solarflare/src/model"
-	"github.com/eynorey/solarflare/src/utils/clock"
 )
 
 // GetStatus returns information on the service and network status
 func GetStatus() model.StatusResponse {
 	cfg := config.Get()
-	bpm, multiplier := clock.GetSpeed()
+	bpm, multiplier := manager.GetClockSpeed()
 
 	return model.StatusResponse{
 		RegisteredServerCount: len(cfg.Servers),
