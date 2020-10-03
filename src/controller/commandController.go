90a78904de432aacfc45d1576d74cbb32fd8493c
Adrian Riedel <adrian.riedel@sap.com>
2020-10-03 20:11:57 +0200

    check for empty command and remove potential leading slash

--- src/controller/commandController.go
+++ src/controller/commandController.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"encoding/json"
 	"net/url"
+	"strings"
 
 	"github.com/eynorey/solarflare/src/client"
 	"github.com/eynorey/solarflare/src/model"
@@ -18,6 +19,12 @@ func RunCommand(values url.Values) error {
 	if err != nil {
 		return sferror.New(sferror.Encoding, "Error parsing data from the command request", err)
 	}
+
+	if commandRequest.Command == "" {
+		return sferror.New(sferror.BadRequest, "Empty command", err)
+	}
+
+	commandRequest.Command = strings.TrimPrefix(commandRequest.Command, "/")
 
 	body, err := json.Marshal(commandRequest)
 	if err != nil {
