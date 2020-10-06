39d0168966a621bd92379782dc8a01887ee59382
Adrian Riedel <adrian.riedel@outlook.com>
2020-10-06 22:58:54 +0200

    refactor single command logic to cut some code

---
+++ src/manager/commandManager.go
@@ -0,0 +1,33 @@
+package manager
+
+import (
+	"encoding/json"
+
+	"github.com/eynorey/solarflare/src/client"
+	"github.com/eynorey/solarflare/src/model"
+	"github.com/eynorey/solarflare/src/utils/sferror"
+)
+
+// RunSingleCommand compiles request for running a single command and executes it on all servers
+func RunSingleCommand(command string) error {
+	body, err := json.Marshal([]string{command})
+	if err != nil {
+		return sferror.New(sferror.Encoding, "Failed to marshal request", err)
+	}
+
+	err = client.ExecuteEffect(commandEffectEndpoint, body)
+
+	update := model.UIUpdate{
+		CommandUpdate: &model.CommandUpdate{
+			Command: command,
+		},
+	}
+
+	if err != nil {
+		update.CommandUpdate.ErrorMessage = err.Error()
+	}
+
+	SendUIUpdate(update)
+
+	return err
+}
