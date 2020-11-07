57fc4494d152a85a02c23536c3aad1eefb15c43f
Adrian Riedel <adrian.riedel@outlook.com>
2020-11-07 21:01:07 +0100

    move formatting options to javascript map

--- src/static/js/uiManager.js
+++ src/static/js/uiManager.js
@@ -23,9 +23,10 @@ var effectDelayMillis = new Array();
 var averageLatency;
 
 // ================ PRELOADING COMPONENTS ================
+var BossbarFormattingMap = new Map();
 
 var LogWindow, StatusSocketConnection, StatusServerCount, StatusLastRequestTime, StatusAverageRequestTime;
-var BossbarForm, BossbarText, BossbarColor, BossbarUpdateButton;
+var BossbarForm, BossbarText, BossbarColor, BossbarUpdateButton, BossbarColorFormatting, BossbarDecorationFormatting;
 var CapsWarning;
 var DetachAllButton, StopEverythingButton, StopAllEffectsButton;
 var ClockIndicator, ClockBPMRangeInput, ClockBPMInput, ClockNoteLengthInput;
@@ -38,6 +39,7 @@ var StopKeyBindings = new Map();
 var TriggerKeyBindings = new Map();
 
 preloadComponents = () => {
+    BossbarFormattingMap = loadFormattingMap();
     LogWindow = document.getElementById("log-window");
     StatusSocketConnection = document.getElementById("status-connection-status");
     StatusServerCount = document.getElementById("status-server-count");
@@ -48,6 +50,8 @@ preloadComponents = () => {
     BossbarText = document.getElementById("bossbar-text");
     BossbarColor = document.getElementById("bossbar-color");
     BossbarUpdateButton = document.getElementById("bossbar-update-button");
+    BossbarColorFormatting = document.getElementById("bossbar-color-formatting");
+    BossbarDecorationFormatting = document.getElementById("bossbar-decoration-formatting");
 
     CapsWarning = document.getElementById("caps-warning");
 
@@ -115,6 +119,8 @@ preloadComponents = () => {
         }
     }
 
+    populateBossbarFormatting();
+
     document.getElementById("loading-overlay").style.display = "none";
 };
 
@@ -177,7 +183,6 @@ toggleFormattingCodePopup = () => {
 
 addFormatting = (code) => {
     var startPosition = BossbarText.selectionStart;
-    var endPosition = BossbarText.selectionEnd;
     var text = BossbarText.value;
 
     text = text.substring(0, startPosition) + code + text.substring(startPosition);
@@ -683,4 +688,47 @@ function getCookie(key) {
         }
     }
     return "";
+}
+
+function loadFormattingMap() {
+    return new Map([
+        ["§0", { "kind": "color", "style": "color:#000000; text-shadow: 1px 1px gray;", "name": "Black" }],
+        ["§1", { "kind": "color", "style": "color: #0000AA", "name": "Dark Blue" }],
+        ["§2", { "kind": "color", "style": "color: #00AA00", "name": "Dark Green" }],
+        ["§3", { "kind": "color", "style": "color: #00AAAA", "name": "Dark Aqua" }],
+        ["§4", { "kind": "color", "style": "color: #AA0000", "name": "Dark Red" }],
+        ["§5", { "kind": "color", "style": "color: #AA00AA", "name": "Dark Purple" }],
+        ["§6", { "kind": "color", "style": "color: #FFAA00", "name": "Gold" }],
+        ["§7", { "kind": "color", "style": "color: #AAAAAA", "name": "Gray" }],
+        ["§8", { "kind": "color", "style": "color: #555555", "name": "Dark Gray" }],
+        ["§9", { "kind": "color", "style": "color: #5555DD", "name": "Blue" }],
+        ["§a", { "kind": "color", "style": "color: #55FF55", "name": "Green" }],
+        ["§b", { "kind": "color", "style": "color: #55FFFF", "name": "Aqua" }],
+        ["§c", { "kind": "color", "style": "color: #FF5555", "name": "Red" }],
+        ["§d", { "kind": "color", "style": "color: #FF55FF", "name": "Light Purple" }],
+        ["§e", { "kind": "color", "style": "color: #FFFF55", "name": "Yellow" }],
+        ["§f", { "kind": "color", "style": "color: #FFFFFF", "name": "White" }],
+
+        ["§k", { "kind": "decoration", "style": "", "name": "ö1%~#n" }],
+        ["§l", { "kind": "decoration", "style": "font-weight: bold", "name": "Bold" }],
+        ["§m", { "kind": "decoration", "style": "text-decoration: line-through", "name": "Strikethrough" }],
+        ["§n", { "kind": "decoration", "style": "text-decoration: underline", "name": "Underline" }],
+        ["§o", { "kind": "decoration", "style": "font-style: italic", "name": "Italic" }],
+        ["§r", { "kind": "decoration", "style": "", "name": "Reset" }]
+    ]);
+}
+
+function populateBossbarFormatting() {
+    for (const [code, formatter] of BossbarFormattingMap) {
+        const { kind, style, name } = formatter;
+
+        switch (kind) {
+            case "color":
+                BossbarColorFormatting.innerHTML += `<span>${code}</span><span onclick='addFormatting("${code}");' style="${style}">${name}</span>`;
+                break;
+            case "decoration":
+                BossbarDecorationFormatting.innerHTML += `<span>${code}</span><span onclick='addFormatting("${code}");' style="${style}">${name}</span>`;
+                break;
+        }
+    }
 }
