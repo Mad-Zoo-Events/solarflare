321be639bcc3e005eb304e7dd758584adca7759a
Adrian Riedel <adrian.riedel@outlook.com>
2020-11-12 22:44:13 +0100

    initialize eslint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
+function App () {
+    return (
+        <div className="App">
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
