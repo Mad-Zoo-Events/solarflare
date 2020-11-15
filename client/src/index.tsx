11c71a2b48870f3b6e4282be548118551e81d16e
Adrian Riedel <adrian.riedel@outlook.com>
2020-11-15 20:15:25 +0100

    implement fontawesome

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 import store from "./store";
+import "./styles/fontawesome";
 
 ReactDOM.render(
     <Provider store={store}>
