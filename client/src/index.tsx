8f9798aea26c1356b77cceed13a182dfbae1f73f
Adrian Riedel <adrian.riedel@outlook.com>
2020-11-15 14:32:03 +0100

    initialize styling

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import "./index.css";
+import "./index.scss";
 import store from "./store";
 
 ReactDOM.render(
