208c04f0f0ddb435b34bf5e0828d700bc5665a1b
Adrian Riedel <adrian.riedel@outlook.com>
2020-11-16 15:54:03 +0100

    make preset modifier dynamic, plug in specific fragments

--- client/src/presetmanager/PresetManager.tsx
+++ client/src/presetmanager/PresetManager.tsx
@@ -1,15 +1,12 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { Preset } from "../domain/presets/Preset";
 import { RootState } from "../RootState";
 import Routes from "../routes";
 import { fetchPresets } from "./PresetManagerActions";
 import PresetManagerList from "./PresetManagerList/PresetManagerList";
 import { PresetManagerProps } from "./PresetManagerProps";
-import * as et from "../domain/EffectType";
-import { CommandPreset } from "../domain/presets";
-import CommandPresetModifier from "./PresetModifier/CommandPresetModifier";
+import PresetModifier from "./PresetModifier/PresetModifier";
 
 const PresetManager = ({
     getPresets,
@@ -17,34 +14,11 @@ const PresetManager = ({
 }: PresetManagerProps) => {
     useEffect(getPresets, []);
 
-    const openPresetModifier = (effectType: string, preset: Preset) => {
-        switch (effectType) {
-        case et.Command:
-            preset = preset as CommandPreset || { id: "", displayName: "", commands: [""] };
-            return <CommandPresetModifier preset={preset} />;
-        case et.Dragon:
-            preset = preset as CommandPreset || { id: "", displayName: "", commands: [""] };
-            return <CommandPresetModifier preset={preset} />;
-        case et.Laser:
-            preset = preset as CommandPreset || { id: "", displayName: "", commands: [""] };
-            return <CommandPresetModifier preset={preset} />;
-        case et.Particle:
-            preset = preset as CommandPreset || { id: "", displayName: "", commands: [""] };
-            return <CommandPresetModifier preset={preset} />;
-        case et.Potion:
-            preset = preset as CommandPreset || { id: "", displayName: "", commands: [""] };
-            return <CommandPresetModifier preset={preset} />;
-        case et.Timeshift:
-            preset = preset as CommandPreset || { id: "", displayName: "", commands: [""] };
-            return <CommandPresetModifier preset={preset} />;
-        }
-    };
-
     return (
         <>
             <Link to={Routes.controlPanel}>Back</Link>
             <PresetManagerList />
-            {presetToEdit && openPresetModifier(presetToEdit.effectType, presetToEdit.preset)}
+            {presetToEdit && <PresetModifier preset={presetToEdit.preset} effectType={presetToEdit.effectType} />}
         </>
     );
 };
