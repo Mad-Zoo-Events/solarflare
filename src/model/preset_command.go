746e625956a61b187dc0a791a1f19eb3b2bf3aae
Adrian Riedel <adrian.riedel@outlook.com>
2020-11-17 01:01:18 +0100

    make retrieve return cmds with objects instead of strings

--- src/model/preset_command.go
+++ src/model/preset_command.go
@@ -11,3 +11,17 @@ type CommandEffectPreset struct {
 	// UI specific models
 	UIKeyBinding string `json:"-" form:"-"`
 }
+
+// CommandEffectPresetResponse is the response model for commands
+type CommandEffectPresetResponse struct {
+	ID          string    `json:"id"`
+	DisplayName string    `json:"displayName"`
+	Description string    `json:"description"`
+	KeyBinding  rune      `json:"keyBinding"`
+	Commands    []Command `json:"commands"`
+}
+
+// Command represents a single command
+type Command struct {
+	CommandString string `json:"command"`
+}
