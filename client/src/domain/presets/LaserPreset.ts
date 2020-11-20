e1d76c41481b00203a14d98f4cbf3ab078fc0a84
Adrian Riedel <adrian.riedel@outlook.com>
2020-11-20 03:27:19 +0100

    implement laser modifier fragment

--- client/src/domain/presets/LaserPreset.ts
+++ client/src/domain/presets/LaserPreset.ts
@@ -1,12 +1,17 @@
 import { IPreset } from "./IPreset";
 
 export interface LaserPreset extends IPreset {
-    isEndLaser: boolean
-    isNonPlayerTargeting: boolean
+    laserType: string
     laserEffects: LaserEffect[]
 }
 
 interface LaserEffect {
-    startPointId: number
-    endPointId: number
+    start: number
+    end: number
 }
+
+export const LaserTypes: Record<string, string> = {
+    end: "End crystal laser connecting the start and end point",
+    nonTargetingGuardian: "Guardian laser connecting the start and end point",
+    targetingGuardian: "Guardian laser which is targeting a player from the start point"
+};
