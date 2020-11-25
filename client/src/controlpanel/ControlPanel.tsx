8ffb0ae3cec2dc51644b11c9a0b4bdb8202d3494
Adrian Riedel <adrian.riedel@outlook.com>
2020-11-25 01:02:57 +0100

    embed legacy control panel into React app

--- client/src/controlpanel/ControlPanel.tsx
+++ client/src/controlpanel/ControlPanel.tsx
@@ -1,9 +1,19 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import PageHeader from "../presetmanager/PageHeader/PageHeader";
+import "./ControlPanel.scss";
 
 const ControlPanel = (): ReactElement => {
+    useEffect(() => {
+        document.getElementById("controlpanel-frame")?.focus();
+    }, []);
+
     return (
-        <PageHeader isControlPanel={true} />
+        <>
+            <PageHeader isControlPanel={true} />
+            <div className="legacy-panel">
+                <iframe id="controlpanel-frame" src={`${window.location.origin}/controlpanel`}></iframe>
+            </div>
+        </>
     );
 };
 
