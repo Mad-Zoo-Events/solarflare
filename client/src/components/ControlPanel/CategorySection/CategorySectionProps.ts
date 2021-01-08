4699facdebc729482f637d7d282e34f72c35dfea
Adrian Riedel <adrian.riedel@outlook.com>
2021-01-08 18:30:55 +0100

    connect stop all buttons to the implementation

--- client/src/components/ControlPanel/CategorySection/CategorySectionProps.ts
+++ client/src/components/ControlPanel/CategorySection/CategorySectionProps.ts
@@ -1,9 +1,13 @@
+import { HandleThunkActionCreator } from "react-redux";
 import { EffectType } from "../../../domain/EffectType";
 import { Preset } from "../../../domain/presets/Preset";
 import { RunningEffect } from "../../../domain/RunningEffect";
+import { stopAll } from "../ControlPanelActions";
 
 export interface CategorySectionProps {
     presets: Preset[]
     runningEffects: RunningEffect[]
     effectType: EffectType
+
+    stopAll: HandleThunkActionCreator<typeof stopAll>
 }
