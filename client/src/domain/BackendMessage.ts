141a5b0efa5a5f0ead09f5e954ec8e4f62b19f26
Adrian Riedel <adrian.riedel@outlook.com>
2021-01-08 20:14:22 +0100

    add model for backend message

---
+++ client/src/domain/BackendMessage.ts
@@ -0,0 +1,57 @@
+import { StopAllOptions } from "./client/StopAllOptions";
+import { ClockAction } from "./ClockAction";
+import { EffectAction } from "./EffectAction";
+import { EffectType } from "./EffectType";
+
+export interface BackendMessage {
+    effectUpdate?: EffectMessage
+    bossbarUpdate?: BossbarMessage
+    clockUpdate?: ClockMessage
+    clockSpeedUpdate?: ClockSpeedMessage
+    statusUpdate?: StatusMessage
+    stageUpdate?: StageMessage
+    commandUpdate?: CommandMessage
+}
+
+export interface EffectMessage {
+    id: string
+    effectType: EffectType
+    displayName: string
+    action: EffectAction
+
+    errorMessage: string
+
+    stopAll?: StopAllOptions
+}
+
+export interface BossbarMessage {
+    text: string
+    color: string
+    action: EffectAction
+}
+
+export interface ClockMessage {
+    id: string
+    effectType: EffectType
+    isOffBeat: boolean
+    action: ClockAction
+}
+
+export interface ClockSpeedMessage {
+    clockSpeedBPM: number
+    clockSpeedMultiplier: number
+}
+
+export interface StatusMessage {
+    activeServerIDs: string[]
+}
+
+export interface StageMessage {
+    selectedStage: string
+}
+
+export interface CommandMessage {
+    command: string
+
+    errorMessage: string
+}
---
+++ client/src/domain/ClockAction.ts
@@ -0,0 +1,4 @@
+export const Subscribe = "subscribe";
+export const Unsubscribe = "unsubscribe";
+
+export type ClockAction = typeof Subscribe | typeof Unsubscribe;
