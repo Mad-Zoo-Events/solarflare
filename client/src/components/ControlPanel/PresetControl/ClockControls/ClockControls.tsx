a53d6a7726ced7a39385602a281529a1c0107f1d
Adrian Riedel <adrian.riedel@outlook.com>
2021-01-13 23:00:11 +0100

    implement clock attachment/detachment from the UI

--- client/src/components/ControlPanel/PresetControl/ClockControls/ClockControls.tsx
+++ client/src/components/ControlPanel/PresetControl/ClockControls/ClockControls.tsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { ReactElement } from "react";
 import { connect } from "react-redux";
+import { Subscribe, Unsubscribe } from "../../../../domain/ClockAction";
 import { RootState } from "../../../../RootState";
 import { handleClockSubscription } from "../../ControlPanelActions";
 import "./ClockControls.scss";
@@ -15,6 +16,7 @@ const ClockControls = ({
 }: ClockControlsProps): ReactElement => {
     const onBeatIcon = onBeatAttached && clockOnBeat ? "fas" : "far";
     const offBeatIcon = offBeatAttached && clockOnBeat ? "far" : "fas";
+    const { isRunning } = subscriptionOptions;
 
     return (
         <div className="clock-controls">
@@ -22,13 +24,19 @@ const ClockControls = ({
                 className="button"
                 icon={[onBeatIcon, "circle"]}
                 size="2x"
-                onClick={() => handleClockSubscription({ ...subscriptionOptions, offBeat: false })}
+                onClick={() => handleClockSubscription({
+                    ...subscriptionOptions,
+                    offBeat: false
+                }, (!isRunning || offBeatAttached) ? Subscribe : Unsubscribe)}
             />
             <FontAwesomeIcon
                 className="button"
                 icon={[offBeatIcon, "circle"]}
                 size="2x"
-                onClick={() => handleClockSubscription({ ...subscriptionOptions, offBeat: true })}
+                onClick={() => handleClockSubscription({
+                    ...subscriptionOptions,
+                    offBeat: true
+                }, (!isRunning || onBeatAttached) ? Subscribe : Unsubscribe)}
             />
         </div>
     );
--- client/src/domain/client/ClockSubscriptionOptions.ts
+++ client/src/domain/client/ClockSubscriptionOptions.ts
@@ -6,5 +6,4 @@ export interface ClockSubscriptionOptions {
         effectType: EffectType
         isRunning: boolean
         offBeat: boolean
-        action: ClockAction
 }
