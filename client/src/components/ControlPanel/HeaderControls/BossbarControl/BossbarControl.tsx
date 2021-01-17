3cb31c31ced3068ff1d01fb9a3cea3721ef60b87
Adrian Riedel <adrian.riedel@outlook.com>
2021-01-17 23:40:55 +0100

    implement formatting popup and improve preview

--- client/src/components/ControlPanel/HeaderControls/BossbarControl/BossbarControl.tsx
+++ client/src/components/ControlPanel/HeaderControls/BossbarControl/BossbarControl.tsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { ChangeEvent, ReactElement, useState } from "react";
 import { connect } from "react-redux";
 import BossbarColor from "../../../../domain/controlpanel/BossbarColor";
+import { FormattingMap } from "../../../../domain/controlpanel/BossbarFormatting";
 import { RootState } from "../../../../RootState";
 import { clearBossbar, setIgnoreKeystrokes, updateBossbar } from "../../ControlPanelActions";
 import { selectBossbarColor, selectBossbarText } from "../../ControlPanelSelectors";
@@ -17,44 +18,76 @@ const BossbarControls = ({
     setIgnoreKeystrokes
 }: BossbarControlProps): ReactElement => {
     const [autoUpdate, setAutoUpdate] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
 
-    const handleColorChange = (e: ChangeEvent<HTMLSelectElement>) => {
-        updateBossbar({
-            color: e.currentTarget.value as BossbarColor,
-            title
-        }, autoUpdate);
-    };
+    const handleColorChange = (e: ChangeEvent<HTMLSelectElement>) => updateBossbar({
+        color: e.currentTarget.value as BossbarColor,
+        title
+    }, autoUpdate);
 
-    const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const title = e.currentTarget.value;
-        updateBossbar({
-            color,
-            title
-        }, autoUpdate && !title.endsWith("§"));
-    };
+    const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => updateBossbar({
+        color,
+        title: e.currentTarget.value
+    }, autoUpdate && !e.currentTarget.value.endsWith("§"));
+
+    const preview = (title.length === 0 || title.match(/^(§.{1}|§)+$/g))
+        ? <input className="bossbar-preview" type="text" placeholder="Now Playing: Bob Ross"/>
+        : <div className="code bossbar-preview fake-input">
+            <BossbarPreview rawText={title} />
+        </div>;
+
+    const input = <input
+        type="text"
+        spellCheck={false}
+        placeholder="Now Playing: Bob Ross"
+        defaultValue={title}
+        onFocus={() => setIgnoreKeystrokes(true)}
+        onBlur={() => { setIgnoreKeystrokes(false); setIsOpen(false); }}
+        onChange={handleTitleChange}
+        autoFocus
+    />;
 
     return (
         <div className="control-panel__bossbar-control">
             <FontAwesomeIcon icon={["fas", "skull"]} size="2x"/>
-            <input
-                type="text"
-                placeholder="Now Playing: Bob Ross"
-                value={title}
-                onFocus={() => setIgnoreKeystrokes(true)}
-                onBlur={() => setIgnoreKeystrokes(false)}
-                onChange={handleTitleChange}
-            />
-            <div className="code bossbar-preview fake-input">
-                <BossbarPreview rawText={title} />
+
+            <div>
+                <div className="button popup-label" onClick={() => { setIgnoreKeystrokes(true); setIsOpen(true); }}>
+                    {isOpen ? input : preview}
+                </div>
+                <div className="popup-holder">
+                    <div className={`popup-content bossbar-popup-content ${isOpen ? "show-popup" : ""}`}>
+                        {preview}
+                        <div className="formatting-group">
+                            <div><div>▼</div><div>Choose color</div></div>
+                            {Array.from(FormattingMap).map(([token, format]) => {
+                                if (format.kind === "decoration") return null;
+                                return <div key={token}>
+                                    <div>{token}</div>
+                                    <div style={format.style}>{format.name}</div>
+                                </div>;
+                            })}
+                        </div>
+                        <div className="formatting-group">
+                            <div><div>▼</div><div>Then formatting</div></div>
+                            {Array.from(FormattingMap).map(([token, format]) => {
+                                if (format.kind === "color") return null;
+                                return <div key={token}>
+                                    <div>{token}</div>
+                                    <div style={format.style}>{format.name}</div>
+                                </div>;
+                            })}
+                        </div>
+                    </div>
+                </div>
             </div>
-            <select
-                value={color}
-                onChange={handleColorChange}
-            >
+
+            <select value={color}onChange={handleColorChange}>
                 {Object.values(BossbarColor).map(key =>
                     <option key={key} value={key}>{key as BossbarColor}</option>
                 )}
             </select>
+
             <div>
                 {!autoUpdate && <FontAwesomeIcon
                     className="button update-button"
@@ -69,6 +102,7 @@ const BossbarControls = ({
                     title="Hide Boss Bar"
                 />
             </div>
+
             <label className="checkbox-container">
                 Update Live
                 <input
