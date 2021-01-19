bcf76003e5e7349c79112f8f8efc0784c8fb7ddb
Adrian Riedel <adrian.riedel@outlook.com>
2021-01-19 19:54:15 +0100

    perform vendor update, remove form parser dependency

--- src/controller/helpers.go
+++ src/controller/helpers.go
@@ -1,15 +1,5 @@
 package controller
 
-import (
-	"github.com/go-playground/form/v4"
-)
-
-var decoder *form.Decoder
-
-func init() {
-	decoder = form.NewDecoder()
-}
-
 func contains(arr []string, str string) bool {
 	for _, e := range arr {
 		if e == str {
