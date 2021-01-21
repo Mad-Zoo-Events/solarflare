fece390d616780566963000ff1c783af83820fd2
Adrian Riedel <adrian.riedel@outlook.com>
2021-01-21 23:56:03 +0100

    rearrange stylesheets + many styling adjustments

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import "./index.scss";
 import store from "./store";
 import "./styles/fontawesome";
+import "./styles/index.scss";
 
 ReactDOM.render(
     <Provider store={store}>
