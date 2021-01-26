849b6315a33d28fe7849f79cfe1ad8303eea17a9
Adrian Riedel <adrian.riedel@outlook.com>
2021-01-26 18:30:04 +0100

    move layout to control panel state

--- client/src/components/ControlPanel/ControlPanelReducer.ts
+++ client/src/components/ControlPanel/ControlPanelReducer.ts
@@ -25,6 +25,14 @@ import { ControlPanelState } from "./ControlPanelState";
 const initialState: ControlPanelState = {
     displayCategories: EffectType.allEffectTypes,
     ignoreKeystrokes: false,
+    layout: [
+        { i: "cat-particle", x: 0, y: 0, w: 5, h: 4, minH: 2 },
+        { i: "cat-potion", x: 6, y: 0, w: 3, h: 2, minH: 2 },
+        { i: "cat-timeshift", x: 6, y: 2, w: 3, h: 2, minH: 2 },
+        { i: "cat-dragon", x: 0, y: 4, w: 4, h: 2, minH: 2 },
+        { i: "cat-laser", x: 4, y: 4, w: 4, h: 2, minH: 2 },
+        { i: "cat-command", x: 0, y: 6, w: 8, h: 2, minH: 2 }
+    ],
     runningEffects: new Map(),
     logEntries: [],
     clock: {
