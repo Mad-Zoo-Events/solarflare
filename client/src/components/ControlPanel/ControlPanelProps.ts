c56e67fd4d693fda3621ced155be1b7c6b2760a3
Adrian Riedel <adrian.riedel@outlook.com>
2021-01-27 20:15:54 +0100

    refactor category grid into own component

--- client/src/components/ControlPanel/ControlPanelProps.ts
+++ client/src/components/ControlPanel/ControlPanelProps.ts
@@ -1,23 +1,18 @@
 import { RefObject } from "react";
-import { Layout } from "react-grid-layout";
 import { HandleThunkActionCreator } from "react-redux";
 import { EffectType } from "../../domain/EffectType";
-import { PresetCollection } from "../../domain/PresetCollection";
 import { Preset } from "../../domain/presets/Preset";
 import { RunningEffect } from "../../domain/RunningEffect";
-import { changeLayout, handleKeyPress } from "./ControlPanelActions";
+import { handleKeyPress } from "./ControlPanelActions";
 
 export interface ControlPanelProps {
-    displayCategories: EffectType[]
-    layout: Layout[]
-    ignoreKeystrokes: boolean
-
-    presets: PresetCollection
     combinedPresets: Preset[]
     runningEffects: Map<string, RunningEffect>
+
+    displayCategories: EffectType[]
+    ignoreKeystrokes: boolean
 
     clockTapButtonRef: RefObject<HTMLDivElement>
 
     handleKeyPress: HandleThunkActionCreator<typeof handleKeyPress>
-    changeLayout: HandleThunkActionCreator<typeof changeLayout>
 }
