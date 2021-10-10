a852e8700f86e4c2abe89ac361b169d133296b16
Kevin Fitzgerald <kevin@kevinfitzgerald.net>
2021-10-10 17:49:23 -0500

    Bypass origin checking for dev environments

--- src/socketHandler.go
+++ src/socketHandler.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 
+	"github.com/eynorey/solarflare/src/config"
 	"github.com/eynorey/solarflare/src/manager"
 	"github.com/eynorey/solarflare/src/utils/sferror"
 )
@@ -15,6 +16,16 @@ func SocketHandler(c *gin.Context) {
 	upgrader := websocket.Upgrader{
 		WriteBufferSize: 131072, // 128 Kb
 	}
+
+	conf := config.Get()
+
+	// No origin issues locally / mac
+	if conf.RunningOnDev {
+		upgrader.CheckOrigin = func(r *http.Request) bool {
+			return true
+		}
+	}
+
 	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
 		sferror.New(sferror.SocketOpen, "Error upgrading connection to websocket", err)
